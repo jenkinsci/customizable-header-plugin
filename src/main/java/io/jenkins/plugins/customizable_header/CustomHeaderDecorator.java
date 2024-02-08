940e002020eae09073152133934ce015a61c3453
Markus Winter <m.winter@sap.com>
2024-02-08 10:46:30 +0100

    do not apply theme on Jenkins Header
    The theme should not be loaded when the Jenkins Header is used. Either
    because the Custom header is disabled or because the Jenkins Header is
    selected globally or via a user setting.
    
    fixes #70

--- src/main/java/io/jenkins/plugins/customizable_header/CustomHeaderDecorator.java
+++ src/main/java/io/jenkins/plugins/customizable_header/CustomHeaderDecorator.java
@@ -2,6 +2,7 @@ package io.jenkins.plugins.customizable_header;
 
 import hudson.Extension;
 import hudson.model.PageDecorator;
+import io.jenkins.plugins.customizable_header.headers.JenkinsHeaderSelector;
 import io.jenkins.plugins.customizable_header.logo.ImageLogo;
 import io.jenkins.plugins.customizable_header.logo.Logo;
 import org.jenkinsci.Symbol;
@@ -27,5 +28,9 @@ public class CustomHeaderDecorator extends PageDecorator {
       return ((ImageLogo) logo).getUrl();
     }
     return "";
+  }
+
+  public boolean isEnabled() {
+    return !(CustomHeaderConfiguration.get().getActiveHeader() instanceof JenkinsHeaderSelector);
   }
 }
